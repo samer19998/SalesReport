fe5fd3fe14eaafa95bdc0cf467f251a7e9f97841
Samer <Samer@172.17.157.43>
2018-04-12 21:44:45 +0900

    Lab09

--- src/main/java/edu/handong/csee/java/salesreport/SalesReport.java
+++ src/main/java/edu/handong/csee/java/salesreport/SalesReport.java
@@ -4,90 +4,114 @@ import java.util.Scanner;
 
 public class SalesReport {
 
+
+	// TODO Auto-generated method stub
+	private double HighestSales;
+	private double averageSales;
+	private Salesman[] team;
+	private int numOfSalesman;
+	//private double getSales;
+
+
+	/**
+	 * 
+	 */
 	
-		// TODO Auto-generated method stub
-		private double highestSales;
-		private double averageSales;
-		private SalesReport[] team;
-		private int numOfSalesman;
-		
-		
-		public SalesReport() {
-			System.out.println("Enter number of salesman");
-			Scanner myScanner = new Scanner(System.in);
-			
-			numOfSalesman = myScanner.nextInt();
-			
-	}
-		public void printOutMySentence() {
-			System.out.println("I want to print out something when i compile");
-			
-		}
-		
-		public static void main(String[] args) {
-			
-			SalesReport myReport = new SalesReport();
-			myReport.getData();
-			myReport.calculate();
-	}
-public void getData() {
-	
-	team = new Salesman[numOfSalesman];
-	
-	for(int i=0; i < numOfSalesman; i++) {
-		
+	public SalesReport() {
+		System.out.println("Enter number of salesman");
 		Scanner myScanner = new Scanner(System.in);
-		
-		System.out.println("Enter data for associate number") + (i+1));
-		
-		System.out.println("Enter name");
-		String name = myScanner.nextline();
-		
-		System.out.println("Enter sales");
-		double sales = myScanner.nextDouble();
-		
-		Salesman mySalesman = newSalesman();
-		mySalesman.setName(name);
-		mySalesman.setSales(sales);
-		
+
+		numOfSalesman = myScanner.nextInt();
+
+	}
+
+
+
+
+	public static void main(String[] args) {
+
+		SalesReport myReport = new SalesReport();
+		myReport.getData();
+		myReport. calculateAverageSales();
+		myReport.highestSales();
+		myReport.printOutResults();
 		
 	}
-	
-}
-public void calculateAverageSales() {
-	
-	double sum = 0;
-	
-	for(int i=0; i <team.length; i++);
-		double sales = team[i].getSales();
-		sum = sum+ sales;
-}
 
-	averageSales = sum/team.length;
-}
 
-public void calculateHighestSales() {
-	
-	for(int i=0; i <team.length-1; i++) {
-		if(team{[i].getSales() < team[i+1].getSales()) {
-			highestSales = team[i+1].getSales();
-		    }
+	public void getData() {
+
+		team = new Salesman[numOfSalesman];
+
+		for(int i=0; i < numOfSalesman; i++) {
+
+			Scanner myScanner = new Scanner(System.in);
+
+			System.out.println("Enter data for associate number" + (i+1));
+
+			System.out.println("Enter name");
+			String name = myScanner.nextLine();
+
+			System.out.println("Enter sales");
+			double sales = myScanner.nextDouble();
+
+			Salesman mySalesman = new Salesman();
+			mySalesman.setmName(name);
+			mySalesman.setSales(sales);
+
+			team[i] = mySalesman;
 		}
-		
+
+	}
+
+
+	public void calculateAverageSales() {
+
+		double sum = 0;
+
+		for(int i = 0 ; i <team.length; i++){
+
+			double sales = team[i].getSales();
+			sum = sum + sales;
+		}
+
+		averageSales = sum /team.length;
+	}
+
+	public void highestSales () {
+
+		for(int i=0; i <team.length-1; i++) {
+
+			if(team[i].getSales()< team[i+1].getSales()) {
+
+				HighestSales = team[i+1].getSales();
+			}
+		}
+
 	}
 	public void printOutResults() {
 		
+
 		System.out.println("Average: " + averageSales);
-		System.out.println("Highest Sales: " + highestSales);
+		System.out.println("Highest Sales: " + HighestSales );
 		
-	    System.out.println("The Highest Sales guy");
-		for(int i = 0; i < team.length; i++) {
-			if(team[i].getSales() == highestSales) {
-				System.out.println("name: " + team[i].getName());
-				System.out.println("Sales: " team[i].getSales());
-			}
+	
+
+	System.out.println("The Highest Sales guy");
+		for(int i = 0; i < team.length; i++) 
+		{
+			if(HighestSales == (team[i].getSales()))
+				
+			{
+			System.out.println("name: " + team[i].getmName());
+			System.out.println("Sales: " +team[i].getSales());
+		
 		}
 	}
-	
-	
+	}
 }
+
+
+
+
+
---
+++ src/main/java/edu/handong/csee/java/salesreport/Salesman.java
@@ -0,0 +1,21 @@
+package edu.handong.csee.java.salesreport;
+
+public class Salesman {
+	
+	private String mName;
+	private double Sales;
+	
+	public String getmName()  {
+		return mName;
+	}
+	public void setmName(String mName1) {
+		mName = mName1;
+	}
+	public double getSales()  {
+		return Sales;
+	}
+	public void setSales(double sales)  {
+		Sales = sales;
+	}
+
+}
