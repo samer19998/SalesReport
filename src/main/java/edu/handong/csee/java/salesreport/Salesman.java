e8981164758949a935df653691ff38a6a4dcf5f6
Samer <Samer@172.17.156.139>
2018-04-21 21:18:54 +0900

    HW1-7

--- src/main/java/edu/handong/csee/java/salesreport/Salesman.java
+++ src/main/java/edu/handong/csee/java/salesreport/Salesman.java
@@ -5,16 +5,21 @@ public class Salesman {
 	private String mName;
 	private double Sales;
 	
-	public String getmName()  {
-		return mName;
+	/**
+	 * this is a class to initialize our main class
+	 * @return
+	 */
+	
+	public String getmName()  {//method
+		return mName;// to return a value
 	}
-	public void setmName(String mName1) {
+	public void setmName(String mName1) {//method
 		mName = mName1;
 	}
-	public double getSales()  {
-		return Sales;
+	public double getSales()  {//method
+		return Sales;// to return a value
 	}
-	public void setSales(double sales)  {
+	public void setSales(double sales)  {//method
 		Sales = sales;
 	}
 
